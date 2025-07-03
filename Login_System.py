0f196d58ed2d5b186e769a5fb0364c46e2a2bbff
HargunKaur <kaurhargun032@gmail.com>
2025-07-02 20:27:51 -0400

    Add files via upload
    updated login system code 
    This is a basic login and registration system using Python and file handling. The program allows users to:
    
    Register a new username and password
    
    Login with their saved credentials
    
    Exit the program
    
    It uses a file called user.txt to store usernames and passwords.

--- Login_System.py
+++ Login_System.py
@@ -1,15 +1,16 @@
 while True:
- print("1. Register \n 2. Login \n 3. Exit")
+ print("\n1. Register \n 2. Login \n 3. Exit")
  choice = input("Select one from the above options: ")
  choice = int(choice)
 
  if choice == 1 :
   username = input("Enter your Username: ")
   password = input("Enter your password make sure it should have 4 digits in it: ")
-  print(" THANKYOU; for entering your detail ")
+  if len(password) != 4:
+    print("\nYou entered a wrong password it must be of 4 digits or should contain numbers only")
+  else:
+   print("THANKYOU! Successfully Registered")
 
-  with open("user.txt","a")as file:
-     file.write("\n"+ username +" : " + password)
  elif choice == 2:
   username = input("Enter your Username: ")
   password = input("Enter your password: ")
@@ -19,14 +20,14 @@ while True:
      login=file.readlines()
      login_info = username + " : " + password
   if login_info in login :
-    print(" LOGIN SUCESSFULL")
+    print(" \nLOGIN SUCESSFULL")
   else:
-    print("You entered a wrong username or password !")
+    print("\nYou entered a wrong username or password !")
  elif choice == 3:
-  print("Thankyou, for choosing us have a great day")  
+  print("\nThankyou, for choosing us have a great day")  
   break
  else:
-  print("you choose an invalid option")
+  print("\nyou choose an invalid option")
 
 
 
